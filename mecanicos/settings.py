806ceb93a2f7a1a96704577c8ab6d9cf80a0cfeb
NicoBust <nic.bustamantes@duocuc.cl>
2024-07-05 01:29:34 -0400

    Initial commit

--- mecanicos/settings.py
+++ mecanicos/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -118,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
---
+++ mecanicos/templates/mecanicos/README.md
@@ -0,0 +1,12 @@
+<h1>PGY3121-007V</h1>
+<h3>evaluacion parcial 2.2</h3>
+
+<h3>Grupo</h3>
+
+<p>Esteban Salas</p>
+<p>Nicolas Bustamante</p>
+<br></br>
+<h4></h4> usuarios en json</h4>
+<p>usuario: esteban, contraseña: 234</p>
+<p>usuario: nicolas, contraseña: 234</p>
+<p>usuario: juan, contraseña: 234</p>

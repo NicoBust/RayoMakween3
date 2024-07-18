f3980110773089c3339abe54f03f1f97cba75c55
NicoBust <nic.bustamantes@duocuc.cl>
2024-07-18 18:51:12 -0400

    Securitizar Key

--- taller/settings.py
+++ taller/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os #agregado
+import environ 
+
+environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #agregado
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$dmfa6(-(7^-)ak7a(lag=s0nkuj+9dfw8pd_4!i=0qok$ec2*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') 
 
 ALLOWED_HOSTS = ['*']
 
